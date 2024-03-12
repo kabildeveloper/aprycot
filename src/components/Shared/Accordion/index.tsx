4aa3a9d8e811049a03ace71afd519d1f6f2bea50
kabildeveloper <kabildeveloper@gmail.com>
2024-03-12 18:22:37 +0530

    Code cleanup

--- src/components/Shared/Accordion/index.tsx
+++ src/components/Shared/Accordion/index.tsx
@@ -30,14 +30,14 @@ export default function Accordion(props: AccordionProps) {
         <>
             <div
                 onClick={onClickToggle}
-                className={`accordion-header mb-2 ${highlightHeader()} ${props.open ? 'w-[80%] mx-auto ps-5' : 'w-[54px] bg-orange-100'}`}>
+                className={`accordion-header  ${highlightHeader()} ${props.open ? 'w-[80%] mx-auto ps-5' : 'w-[54px] bg-orange-100'}`}>
                 <div className={`flex items-center ${props.open ? '': 'justify-center'} gap-2 w-full`}>
                     <props.icon className='icon text-orange-500' style={{fontSize: '22px'}}/>
                     {props.open && <p>{props.title}</p>}
                 </div>
                 {props.open && <RiArrowRightSLine className='text-gray-900 me-1' style={{fontSize: '22px'}}/>}
             </div>
-            {props.open && <div className={`w-full transition-all duration-500 ps-[70px] mt-1 mb-2 overflow-hidden ${expand ? 'max-h-[200px]' : 'max-h-0'}`}>
+            {props.open && <div className={`accordion-expansion ${expand ? 'max-h-[200px]' : 'max-h-0'}`}>
                 <ul>
                     {
                         props.subMenu.map((menu)=>{
@@ -45,11 +45,8 @@ export default function Accordion(props: AccordionProps) {
                                 <li
                                     onClick={()=>onCLickNavLink(menu.link)}
                                     key={menu.title}
-                                    className={`${isPathMatch(menu.link) ? '!bg-orange-500 !text-white [&>*]:!text-white' : ''} 
-                                                py-2 flex gap-4 items-center rounded-full hover:bg-orange-100 
-                                                [&>*]:hover:text-orange-500 ps-4 me-4 transition-all duration-200 cursor-pointer`}>
-
-                                    <RiCircleFill className='text-orange-500 text-xs'/> <p
+                                    className={`accordion-list ${isPathMatch(menu.link) ? '!bg-orange-500 !text-white [&>*]:!text-white' : ''}`}>
+                                        <RiCircleFill className='text-orange-500 text-xs'/> <p
                                     className='text-[16px]'>{menu.title}</p></li>
                             )
                         })
